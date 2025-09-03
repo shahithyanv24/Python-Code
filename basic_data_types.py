6d32807c159a034e9f775cd12b1ce8ec26b30f85
shahithyanv24 <shahithyan.v24@gmail.com>
2025-09-02 20:11:21 -0500

    added subtraction,division, and remainders

--- basic_data_types.py
+++ basic_data_types.py
@@ -4,6 +4,18 @@ def add_numbers(num1, num2):
 def multiply_numbers(num1, num2):
     return num1 * num2
 
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def divide_numbers(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero"
+    
+def remainder_numbers(num1, num2):
+    return num1 % num2
+
 x = 7
 print(type(x))  # <class 'int'>
 print(x)
@@ -17,11 +29,24 @@ print("The sum of", x, "and", b, "is", sum)
 d = 6
 s = 10
 
+c=10
+e=5
+f=4
+
 sum2 = add_numbers(d, s)
 product2 = multiply_numbers(d, s)
 print("The product of", d, "and", s, "is", product2)
 print("The product of", product2, "and", product, "is", multiply_numbers(product2, product))
 print("The sum of", d, "and", s, "is", sum2)
+
+difference = subtract_numbers(s, d)
+print("The difference of", s, "and", d, "is", difference)
+
+quotient = divide_numbers(c, e)
+print("The quotient of", c, "and", e, "is", quotient)
+
+rem=remainder_numbers(c, f)
+print("The remainder of", c, "and", f, "is", rem)
 
 y = "Shahithyan"
 print(type(y))  # <class 'str'>
