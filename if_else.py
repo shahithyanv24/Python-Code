0cdde5b81f54a660ab3a496848c8625692db7661
shahithyanv24 <shahithyan.v24@gmail.com>
2025-09-22 18:37:24 -0500

    if/else

---
+++ if_else.py
@@ -0,0 +1,25 @@
+a = 6
+b = 7
+c = 10
+d = 10
+
+if b > a:
+    print("b is greater than a")
+elif c == d:
+    print("a is equal to b")
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+
+if b > a: print("a is greater than b")
+
+if a == b and c == d:
+    print("Both conditions are True")
+
+if a == b or c == d:
+    print("At least one of the conditions is True")
+
+if not a > b:
+    print("a is not greater than b")
