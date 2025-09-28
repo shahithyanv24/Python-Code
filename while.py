a6e9c0a8ac5946d07ca615facccb6914c5f5f112
shahithyanv24 <shahithyan.v24@gmail.com>
2025-09-28 16:37:14 -0500

    while loops

---
+++ while.py
@@ -0,0 +1,20 @@
+i = 1
+while i < 6:
+  print(i)
+  if i == 7:
+    break
+  i += 1
+
+x = 0
+while x < 6:
+  x += 1
+  if x == 3:
+    continue
+  print(x)
+
+  y = 1
+while y < 6:
+  print(y)
+  y += 1
+else:
+  print("i is no longer less than 6")
