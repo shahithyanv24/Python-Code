dd51efa8f867fdddb966724a501e8d4fc7ecae91
shahithyanv24 <shahithyan.v24@gmail.com>
2025-10-05 17:58:47 -0500

    function

---
+++ function.py
@@ -0,0 +1,14 @@
+def world():
+    print("Hello, World!")
+
+world()
+
+def name(name):
+    print(name + " " "Vijayaraj")
+
+name("Shahithyan")
+
+def lakers(*players):
+    print("Lakers players are:" " " + players[0] + ", " + players[1] + ", " + players[2] + ", " + players[3])
+
+lakers("LeBron", "Austin", "Luka", "Deandre")
