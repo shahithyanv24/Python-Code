d07e2228b167e22ca5e67d5410a150d1f7af9885
shahithyanv24 <shahithyan.v24@gmail.com>
2025-10-11 16:28:37 -0500

    range

---
+++ range.py
@@ -0,0 +1,28 @@
+x = range(5)
+print(x)
+print(list(x))
+print(x[2])
+
+y = range(3, 10)
+print(y)
+print(list(y))
+print(y[4])
+
+z = range(3, 10, 2)
+print(z)
+print(list(z))
+print(z[2])
+
+for i in range(5):
+    print(i)
+
+print(list(range(5)))
+print(list(range(3, 10)))
+print(list(range(3, 10, 2)))
+
+print(6 in y)
+print(50 in y)
+
+print(len(x))
+print(len(y))
+print(len(z))
